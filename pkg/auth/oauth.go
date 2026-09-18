2157b9a5b37cc2b216b39fbe324bc78ba302325e
thahzell <taco.xr@proton.me>
2026-09-18 14:30:14 -0500

    refactor(auth): use build directives for browser opening

--- pkg/auth/oauth.go
+++ pkg/auth/oauth.go
@@ -11,8 +11,6 @@ import (
 	"net"
 	"net/http"
 	"net/url"
-	"os/exec"
-	"runtime"
 	"strings"
 	"time"
 
@@ -271,22 +269,4 @@ func exchangeCode(cfg *config.Config, code, verifier, redirectURI string) (*Toke
 	}
 
 	return &tr, nil
-}
-
-func openBrowser(url string) error {
-	var cmd string
-	var args []string
-
-	switch runtime.GOOS {
-	case "windows":
-		cmd = "rundll32"
-		args = []string{"url.dll,FileProtocolHandler", url}
-	case "darwin":
-		cmd = "open"
-		args = []string{url}
-	default: // "linux", "freebsd", "openbsd", "netbsd"
-		cmd = "xdg-open"
-		args = []string{url}
-	}
-	return exec.Command(cmd, args...).Start()
 }
